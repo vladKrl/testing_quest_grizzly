addebcbeedd8cfbd1a4fbaa292950fd8d57d079b
vladKrl <vladkurlenia@gmail.com>
2023-10-18 09:30:43 +0300

    New code

--- index.php
+++ index.php
@@ -5,19 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test-Quest</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="js/main.js"></script>
+
 <body>
     <form action="" method="get">
         <label for="phone">Enter number phone you need: </label>
         <input type="tel" id="phone" name="phone">
     </form>
+
+    <div class="cookie-popup pt-3">
+        <div class="text">
+            This page uses cookie. Accept it please.
+        </div>
+        <div class="mt-3 d-flex flex-row justify-content-around buttons">
+            <button class="close-btn">Close</button>
+            <button class="accept-btn">Accept</button>
+        </div>
+    </div>
 </body>
 </html>
+
 
 <?php
 
 $ch = curl_init("https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json");
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    if (Boolean(document.cookie.split(';')[0].split('=')[1]) == true) {
+      $(".cookie-popup").addClass("d-none");
+    }
+    let date = new Date();
+    date.setHours(date.getHours() + 24);
+    date = date.toUTCString();
+    $(".accept-btn").on("click", function () {
+        document.cookie = "accepted=True ; expires=" + date;
+        $(".cookie-popup").addClass("d-none");
+    })
+
+    $(".close-btn").on("click", function () {
+      $(".cookie-popup").addClass("d-none");
+    });
+    console.log(Boolean(document.cookie.split(';')[0].split('=')[1]));
+  });
